95472f1f9d8e1c2c4f91f230b85affa5f0511472
ferdinand-dhbw <inf19211@lehre.dhbw-stuttgart.de>
2020-07-10 13:14:13 +0200

    Add converter

---
+++ javascript/converter.js
@@ -0,0 +1,17 @@
+function myFunction() {
+	let form = document.getElementById("myForm");
+	
+	let string= "{\n\"name\": \"" + form.elements[0].value + "\",\n";
+	string += "\"location\": \"" + form.elements[1].value + "\",\n";
+	string += "\"organizer\": \"" + form.elements[2].value + "\",\n";
+	string += "\"start\": \"" + form.elements[3].value + "\",\n";
+	string += "\"end\": \"" + form.elements[4].value + "\",\n";
+	string += "\"status\": \"" + form.elements[5].value + "\",\n";
+	string += "\"allday\": " + form.elements[6].checked + ",\n";
+	string += "\"webpage\": \"" + form.elements[7].value + "\",\n";
+	string += "\"imagedata\": \"" + form.elements[8].value + "\",\n";
+	string += "\"categories\": [],\n";
+	string += "\"extra\": \"" + form.elements[10].value + "\"\n}";
+	
+	return JSON.parse(string);
+}
