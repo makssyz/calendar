21e90a3d6a87ec699012c90b5108a4a5e93b170e
Makss Golubs <makss.golubs@ibm.com>
2020-08-01 23:24:04 +0200

    Update backend and finalize buttons

--- javascript/event-editor.js
+++ javascript/event-editor.js
@@ -1,5 +1,5 @@
 
-function editCalendarEvent(calendarEvent) {
+function editCreateFormToUpdateForm(calendarEvent) {
 
     setCookie("event", JSON.stringify(calendarEvent), 1)
 
@@ -17,20 +17,9 @@ function changeButton (calendarEvent) {
     let form = document.getElementById("createEventForm");
     let button = document.getElementById("createEventButton");
 
-    // let calendarEvent = getCookie("event");
-
-    console.log(calendarEvent);
-
-
-    console.log(form)
-    console.log(button)
-
-    // form.action = "updateCalendarEvent(" + url + "," + username + "," + calendarEvent.id + "," + JSON.stringify(calendarEvent) + ")";
+    form.action = "javascript:editCalendarEventInView(convertFormToObject('createEventForm'), " + calendarEvent.id + ")";
     button.className = "btn btn-primary";
     button.innerText = "Update!";
-
-    console.log(form)
-    console.log(button)
 }
 
 function loadEventIntoForm(calendarEvent) {
@@ -42,7 +31,6 @@ function loadEventIntoForm(calendarEvent) {
     document.getElementById("status").value = calendarEvent.status;
     document.getElementById("allday").value = calendarEvent.allday;
     document.getElementById("webpage").value = calendarEvent.webpage;
-    document.getElementById("imageurl").value = calendarEvent.imageurl;
     document.getElementById("categories").value = calendarEvent.categories;
     document.getElementById("extra").value = calendarEvent.extra;
 }
