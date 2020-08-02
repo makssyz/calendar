8953e927bfad8de8694a5840f046f62a075e6a02
Makss Golubs <makss.golubs@ibm.com>
2020-08-02 20:51:50 +0200

    Validate JS

--- javascript/display-manager.js
+++ javascript/display-manager.js
@@ -1,30 +1,32 @@
-let listViewPage = document.getElementById("listViewPage")
-let monthViewPage = document.getElementById("monthViewPage")
-let formPage = document.getElementById("formPage")
-let successMessage = document.getElementById("successMessage")
+/*jshint esversion: 6 */
+
+let listViewPage = document.getElementById("listViewPage");
+let monthViewPage = document.getElementById("monthViewPage");
+let formPage = document.getElementById("formPage");
+let successMessage = document.getElementById("successMessage");
 
 function displayListViewPage() {
-    clearAll()
-    listViewPage.style.display = "inline"
+    clearAll();
+    listViewPage.style.display = "inline";
 }
 
 function displayMonthViewPage() {
-    clearAll()
-    monthViewPage.style.display = "inline"
+    clearAll();
+    monthViewPage.style.display = "inline";
 }
 
 function displayFormPage() {
-    clearAll()
-    formPage.style.display = "inline"
+    clearAll();
+    formPage.style.display = "inline";
 }
 
 function showSuccessMessage() {
-    successMessage.style.display = "inline"
+    successMessage.style.display = "inline";
 }
 
 function clearAll() {
-    listViewPage.style.display = "none"
-    monthViewPage.style.display = "none"
-    formPage.style.display = "none"
-    successMessage.style.display = "none"
+    listViewPage.style.display = "none";
+    monthViewPage.style.display = "none";
+    formPage.style.display = "none";
+    successMessage.style.display = "none";
 }
