6a19d2c3eb0fd5c26f7073aff1107278b6b971bc
ferdinand-dhbw <inf19211@lehre.dhbw-stuttgart.de>
2020-08-03 11:23:32 +0200

    Add image.js

--- javascript/image.js
+++ javascript/image.js
@@ -1,9 +1,10 @@
-function uploadImage(id, imagePicker){
+function uploadImage(id, imagePickerId){
+	let imagePicker=document.getElementById(imagePickerId);
 	if(imagePicker.files && imagePicker.files[0]){
 		const fr = new FileReader();
 		fr.readAsDataURL(imagePicker.files[0]);
 		fr.onload = (function (data) {
-			addImageToEvent(url, user, id, {imagedata: fr.result});
+			addImageToEvent(url, user, id, {imagedata: await fr.result});
 		}
 	}
 }
