774ae9bc12d3af2fb78beee0070a2406c897fa9b
ferdinand-dhbw <inf19211@lehre.dhbw-stuttgart.de>
2020-08-03 13:48:11 +0200

    Validate monthly-view.js

--- javascript/monthly-view.js
+++ javascript/monthly-view.js
@@ -1,13 +1,4 @@
-function addEventToMonthlyHtmlString(htmlElement, calendarEvent) {
-htmlElement.innerHTML +=
-	"<div class=\"card-body\">\n" +
-	createTableString(calendarEvent) +
-	createButtonString(calendarEvent) +
-	"</div>\n";
-}
-
-
-//----------------------------
+/*jshint esversion:6*/
 const months = [ 
 		"January", 
 		"February", 
@@ -24,7 +15,7 @@ const months = [
 ];
 
 // Given data for events in JSON format
-var event_data = {
+let event_data = {
 	"events": []
 };
 
@@ -32,8 +23,7 @@ var event_data = {
 $(document).ready(preInit(new Date()));
 
 function preInit(date){
-	//var date = new Date();
-	var today = date.getDate();
+	let today = date.getDate();
 
 	// Set click handlers for DOM elements
 	$(".right-button").click({date: date}, next_year);
@@ -44,7 +34,7 @@ function preInit(date){
 	// Set current month as active
 	$(".months-row").children().eq(date.getMonth()).addClass("active-month");
 	init_calendar(date);
-	var events = check_events(today, date.getMonth()+1, date.getFullYear());
+	let events = check_events(today, date.getMonth()+1, date.getFullYear());
 	show_events(events, months[date.getMonth()], today);
 }
 
@@ -52,21 +42,21 @@ function preInit(date){
 function init_calendar(date) {
 	$(".tbody").empty();
 	$(".events-container").empty();
-	var calendar_days = $(".tbody");
-	var month = date.getMonth();
-	var year = date.getFullYear();
-	var day_count = days_in_month(month, year);
-	var row = $("<tr class='table-row'></tr>");
-	var today = date.getDate();
+	let calendar_days = $(".tbody");
+	let month = date.getMonth();
+	let year = date.getFullYear();
+	let day_count = days_in_month(month, year);
+	let row = $("<tr class='table-row'></tr>");
+	let today = date.getDate();
 	// Set date to 1 to find the first day of the month
 	date.setDate(1);
-	var first_day = date.getDay();
+	let first_day = date.getDay();
 	// 35+firstDay is the number of date elements to be added to the dates table
 	// 35 is from (7 days in a week) * (up to 5 rows of dates in a month)
-	for(var i=0; i<35+first_day; i++) {
+	for(let i=0; i<35+first_day; i++) {
 		// Since some of the elements will be blank,
 		// need to calculate actual date from index
-		var day = i-first_day+1;
+		let day = i-first_day+1;
 		// If it is a sunday, make a new row
 		if(i%7===0) {
 			calendar_days.append(row);
@@ -74,12 +64,12 @@ function init_calendar(date) {
 		}
 		// if current index isn't a day in this month, make it blank
 		if(i < first_day || day > day_count) {
-			var curr_date = $("<td class='table-date nil'>"+"</td>");
+			let curr_date = $("<td class='table-date nil'>"+"</td>");
 			row.append(curr_date);
 		}
 		else {
-			var curr_date = $("<td class='table-date'>"+day+"</td>");
-			var events = check_events(day, month+1, year);
+			let curr_date = $("<td class='table-date'>"+day+"</td>");
+			let events = check_events(day, month+1, year);
 			if(today===day && $(".active-date").length===0) {
 				curr_date.addClass("active-date");
 				show_events(events, months[month], day);
@@ -100,8 +90,8 @@ function init_calendar(date) {
 
 // Get the number of days in a given month/year
 function days_in_month(month, year) {
-	var monthStart = new Date(year, month, 1);
-	var monthEnd = new Date(year, month + 1, 1);
+	let monthStart = new Date(year, month, 1);
+	let monthEnd = new Date(year, month + 1, 1);
 	return (monthEnd - monthStart) / (1000 * 60 * 60 * 24);
 }
 
@@ -112,16 +102,16 @@ function date_click(event) {
 	$(".active-date").removeClass("active-date");
 	$(this).addClass("active-date");
 	show_events(event.data.events, event.data.month, event.data.day);
-};
+}
 
 // Event handler for when a month is clicked
 function month_click(event) {
 	$(".events-container").show(250);
 	$("#dialog").hide(250);
-	var date = event.data.date;
+	let date = event.data.date;
 	$(".active-month").removeClass("active-month");
 	$(this).addClass("active-month");
-	var new_month = $(".month").index(this);
+	let new_month = $(".month").index(this);
 	date.setMonth(new_month);
 	init_calendar(date);
 }
@@ -135,8 +125,8 @@ function onTabMonth() {
 // Event handler for when the year right-button is clicked
 function next_year(event) {
 	$("#dialog").hide(250);
-	var date = event.data.date;
-	var new_year = date.getFullYear()+1;
+	let date = event.data.date;
+	let new_year = date.getFullYear()+1;
 	$("year").html(new_year);
 	date.setFullYear(new_year);
 	init_calendar(date);
@@ -145,8 +135,8 @@ function next_year(event) {
 // Event handler for when the year left-button is clicked
 function prev_year(event) {
 	$("#dialog").hide(250);
-	var date = event.data.date;
-	var new_year = date.getFullYear()-1;
+	let date = event.data.date;
+	let new_year = date.getFullYear()-1;
 	$("year").html(new_year);
 	date.setFullYear(new_year);
 	init_calendar(date);
@@ -159,19 +149,19 @@ function show_events(events, month, day) {
 	$(".events-container").show(250);
 	// If there are no events for this date, notify the user
 	if(events.length===0) {
-		var event_card = $("<div class='event-card'></div>");
-		var event_name = $("<div class='event-name'>There are no events planned for "+month+" "+day+".</div>");
+		let event_card = $("<div class='event-card'></div>");
+		let event_name = $("<div class='event-name'>There are no events planned for "+month+" "+day+".</div>");
 		$(event_card).css({ "border-left": "10px solid #FF1744" });
 		$(event_card).append(event_name);
 		$(".events-container").append(event_card);
 	}
 	else {
 		// Go through and add each event as a card to the events container
-		for(var i=0; i<events.length; i++) {
-			var event_card = $("<div class='event-card' id=event-card-" + events[i].id + "></div>");
-			var event_name = $("<div class='event-name'>"+events[i]["title"]+":</div>");
+		for(let i=0; i<events.length; i++) {
+			let event_card = $("<div class='event-card' id=event-card-" + events[i].id + "></div>");
+			let event_name = $("<div class='event-name'>"+events[i].title + ":</div>");
 			let idString = "tableContainer" + i;
-			var container = $("<div class='accordion' id=" + idString +  "></div>");
+			let container = $("<div class='accordion' id=" + idString +  "></div>");
 			$(event_card).append(event_name).append(container);//.append(event_count);
 			$(".events-container").append(event_card);
 			addEventToMonthlyHtmlString(document.getElementById(idString), events[i]);
@@ -181,31 +171,39 @@ function show_events(events, month, day) {
 
 // Checks if a specific date has any events
 function check_events(day, month, year) {
-	var events = [];
-	for(var i=0; i<event_data["events"].length; i++) {
-		var event = event_data["events"][i];
-		if((new Date(event["start"])).getDate()===day &&
-			(new Date(event["start"])).getMonth()+1===month &&
-			(new Date(event["start"])).getYear()+1900===year) {
+	let events = [];
+	for(let i=0; i<event_data.events.length; i++) {
+		let event = event_data.events[i];
+		if((new Date(event.start)).getDate()===day &&
+			(new Date(event.start)).getMonth()+1===month &&
+			(new Date(event.start)).getYear()+1900===year) {
 				events.push(event);
 			}
 	}
 	return events;
 }
 
+function addEventToMonthlyHtmlString(htmlElement, calendarEvent) {
+htmlElement.innerHTML +=
+	"<div class=\"card-body\">\n" +
+	createTableString(calendarEvent) +
+	createButtonString(calendarEvent) +
+	"</div>\n";
+}
+
 function deleteCalendarEventInMonthlyView(id){
 	let deleted = document.getElementById("event-card-" + id);
 	deleted.parentElement.removeChild(deleted);
-	const date = new Date(event_data["events"].find(element => element.id == id).start);
-	event_data["events"] = event_data["events"].filter(event => event.id != id);
+	const date = new Date(event_data.events.find(element => element.id == id).start);
+	event_data.events = event_data.events.filter(event => event.id != id);
 	preInit(date);
 }
 
 //fill events
 function updateCalendar(host, username) {
 	getCalendarEvents(host, username).then(function (data) {
-		event_data["events"]=data;
+		event_data.events=data;
 	  }).catch(function (e) {
-		console.warn(e)
+		console.warn(e);
 	  });
 }
